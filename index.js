f020363cf96733affd6d40ef310980bb3585d11f
Saima Khan <saimakhancsedu@gmail.com>
2025-12-06 16:57:17 +0600

    middleware added

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 
+//middleware
+
+app.use(express.json());
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("Book Courier Server is Running...");
 });
